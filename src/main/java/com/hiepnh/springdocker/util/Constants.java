3eb89a6080619d6a5b2edc71ba8b2d685a001818
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-10-18 15:10:51 +0700

    added utils

---
+++ src/main/java/com/hiepnh/springdocker/util/Constants.java
@@ -0,0 +1,7 @@
+package com.hiepnh.springdocker.util;
+
+public class Constants {
+    public static final String CUSTOMER_NOT_FOUND = "CUSTOMER_NOT_FOUND";
+    public static final String PHONE_NUMBER_ALREADY_EXISTS = "PHONE_NUMBER_ALREADY_EXISTS";
+    public static final String CUSTOMER_MUST_BE_AT_LEAST_18_YEARS_OLD = "CUSTOMER_MUST_BE_AT_LEAST_18_YEARS_OLD";
+}
